592dae674c263bd7ba97c991adba57f557b3ebb3
Wippit.it <vanha.jungle@gmail.com>
2020-03-24 08:48:11 +0700

    Add pro jum for horse

--- Horse.js
+++ Horse.js
@@ -2,14 +2,9 @@ function Horse(name) {
 	this.name = name;
 }
 
-
-
-
-
-
-
-
-
+Horse.prototype.jum = function () {
+	console.log('juming...');
+}
 
 Horse.prototype.run = function () {
 	console.log('runing...');
